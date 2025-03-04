f0a3ab2072e3ca2a9fd6891910d2f5cbdb6fdb7b
novakvova <novakvova@gmail>
2025-03-04 17:56:17 +0200

    test program

--- PostgresProject/PostgresProject/Program.cs
+++ PostgresProject/PostgresProject/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Text;
+using Animal.Infrastructure;
+using Animal.Infrastructure.Entities;
+using Animal.Infrastructure.Services;
 
 namespace PostgresProject
 {
@@ -9,7 +12,13 @@ namespace PostgresProject
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
             Console.WriteLine("Передаю вітання нашим друзям Броненосцям. :)!");
+            var context = new AppAnimalContext();
 
+            var myRep = new Repository<Specie>(context);
+            var entity = new Specie();
+            entity.Name = "Кіт";
+            myRep.Add(entity);
+            myRep.SaveChanges();
         }
     }
 }
