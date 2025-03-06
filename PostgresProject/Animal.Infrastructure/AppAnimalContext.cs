d5f6f631826f8a9c849cde34ca9727962a44c1fd
novakvova <novakvova@gmail>
2025-03-06 14:53:12 +0200

    Add Dependecy Injection

--- PostgresProject/Animal.Infrastructure/AppAnimalContext.cs
+++ PostgresProject/Animal.Infrastructure/AppAnimalContext.cs
@@ -5,11 +5,15 @@ namespace Animal.Infrastructure
 {
     public class AppAnimalContext : DbContext
     {
+        public AppAnimalContext() { }
         public DbSet<Specie> Species { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseNpgsql("Server=localhost;Database=AnimalDb;User Id=postgres;Password=123456;");
             optionsBuilder.UseNpgsql("Host=ep-damp-cell-a2taj2ik-pooler.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_TnC7QlaA3bWx");
         }
+
+        public AppAnimalContext(DbContextOptions<AppAnimalContext> options) : 
+            base(options) { }
     }
 }
--- PostgresProject/PostgresProject/Program.cs
+++ PostgresProject/PostgresProject/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System.Text;
 using Animal.Infrastructure;
 using Animal.Infrastructure.Entities;
+using Animal.Infrastructure.Interfaces;
 using Animal.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace PostgresProject
 {
@@ -9,16 +13,42 @@ namespace PostgresProject
     {
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.Unicode;
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.WriteLine("Передаю вітання нашим друзям Броненосцям. :)!");
-            var context = new AppAnimalContext();
+            var host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddDbContext<AppAnimalContext>(options =>
+                            options.UseNpgsql("Host=ep-damp-cell-a2taj2ik-pooler.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_TnC7QlaA3bWx"));
 
-            var myRep = new Repository<Specie>(context);
-            var entity = new Specie();
-            entity.Name = "Кіт";
-            myRep.Add(entity);
-            myRep.SaveChanges();
+                        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+                    })
+                    .Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var serviceProvider = scope.ServiceProvider;
+            var specieRepository = serviceProvider.GetRequiredService<IRepository<Specie>>();
+
+            // Додаємо новий запис
+            specieRepository.Add(new Specie { Name = "Lion" });
+            specieRepository.SaveChanges();
+
+            // Отримуємо всі записи
+            var species = specieRepository.GetAll();
+            foreach (var specie in species)
+            {
+                Console.WriteLine($"ID: {specie.Id}, Name: {specie.Name}");
+            }
+
+            //Console.InputEncoding = Encoding.Unicode;
+            //Console.OutputEncoding = Encoding.Unicode;
+            //Console.WriteLine("Передаю вітання нашим друзям Броненосцям. :)!");
+            //var context = new AppAnimalContext();
+
+            //var myRep = new Repository<Specie>(context);
+            //var entity = new Specie();
+            //entity.Name = "Кіт";
+            //myRep.Add(entity);
+            //myRep.SaveChanges();
         }
     }
 }
