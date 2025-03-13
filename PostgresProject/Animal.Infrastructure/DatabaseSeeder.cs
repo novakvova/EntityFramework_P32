1d97b63cc44b30694202f519029f218c41c1bdcd
novakvova <novakvova@gmail>
2025-03-13 17:31:58 +0200

    Seed Data is good

--- PostgresProject/Animal.Infrastructure/DatabaseSeeder.cs
+++ PostgresProject/Animal.Infrastructure/DatabaseSeeder.cs
@@ -1,4 +1,5 @@
 ﻿using Animal.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Animal.Infrastructure;
 
@@ -6,6 +7,8 @@ public class DatabaseSeeder(AppAnimalContext context)
 {
     public void SeedData()
     {
+        context.Database.Migrate();
+        
         if(!context.ShelterLocations.Any())
         {
             var kyiv = new ShelterLocation
@@ -22,6 +25,43 @@ public class DatabaseSeeder(AppAnimalContext context)
                 Phone = "+380671234567"
             };
             context.ShelterLocations.Add(lviv);
+            context.SaveChanges();
+        }
+
+        if(!context.Species.Any())
+        {
+            context.Species.Add(new Specie { Name = "Кіт" });
+            context.Species.Add(new Specie { Name = "Собака" });
+            context.Species.Add(new Specie { Name = "Білка" });
+            context.Species.Add(new Specie { Name = "Бобер" });
+            context.SaveChanges();
+        }
+
+        if (!context.Animals.Any())
+        {
+            var cat = new AnimalEntity
+            {
+                Name = "Барсик",
+                SpecieId = 1,
+                Breed = "Персидський",
+                Age = 3,
+                Gender = "Чоловіча",
+                ArrivalDate = DateOnly.FromDateTime(DateTime.Now.AddMonths(-2)),
+                ShelterLocationId = 1
+            };
+            context.Animals.Add(cat);
+
+            var dog = new AnimalEntity
+            {
+                Name = "Рекс",
+                SpecieId = 2,
+                Breed = "Німецька вівчарка",
+                Age = 5,
+                Gender = "Чоловіча",
+                ArrivalDate = DateOnly.FromDateTime(DateTime.Now.AddYears(-1)),
+                ShelterLocationId = 2
+            };
+            context.Animals.Add(dog);
             context.SaveChanges();
         }
     }
