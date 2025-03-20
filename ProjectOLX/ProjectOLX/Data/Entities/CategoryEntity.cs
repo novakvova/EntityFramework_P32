af5a8412c27d6adebde3a4f696edfa6b56a1b522
novakvova <novakvova@gmail>
2025-03-20 16:49:16 +0200

    Begin working Category

---
+++ ProjectOLX/ProjectOLX/Data/Entities/CategoryEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectOLX.Data.Entities;
+
+[Table("tblCategories")]
+public class CategoryEntity : BaseEntity<long>
+{
+    [Required, StringLength(255)]
+    public string Name { get; set; } = string.Empty;
+    [Required, StringLength(255)]
+    public string UrlSlug { get; set; } = string.Empty;
+
+    public int Priority { get; set; }
+
+    [StringLength(200)]
+    public string? Image { get; set; }
+
+    [StringLength(40000)]
+    public string? Description { get; set; }
+
+}
---
+++ ProjectOLX/ProjectOLX/Data/OlxContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectOLX.Data.Entities;
+
+namespace ProjectOLX.Data
+{
+    public class OlxContext : DbContext
+    {
+        public DbSet<CategoryEntity> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=ep-lively-snowflake-a2h0k1on-pooler.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_NHJoc4p2gzWA");
+        }
+    }
+}
